6acf9329fd5d528379dacf64eb9e9a5bcc2bc093
NAVEENKUMARMATURI <nmz28@mst.edu>
2024-03-25 20:31:34 -0500

    Update Home Section

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -10,14 +10,14 @@ const HomePage = () => {
       <HeroSection
         title="Buy Iphone 14 Pro"
         subtitle="Experience the power of the latest iphone 14 with our most Pro Camera ever."
-        link="/product/66005465163e8fa0a3c76ce3"
+        link="/product/6601fffab8bf02abd8df3ba4"
         image={iphone}
       />
       <FeaturedProducts />
       <HeroSection
         title="Build the ultimate setup"
         subtitle="You can add Studio Display and color-matched Magic accessories to your bag after configure you Mac mini."
-        link="/product/66005465163e8fa0a3c76ceb"
+        link="/product/6601fffab8bf02abd8df3bac"
         image={mac}
       />
     </div>
